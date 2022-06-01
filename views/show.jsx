60738511bd044de8f6ec1f2f5877bacc1bcd0a87
unknown <gespejo2144@gmail.com>
2022-05-31 20:48:29 -0700

    edit and update routes complete

--- views/show.jsx
+++ views/show.jsx
@@ -24,6 +24,11 @@ function Show ({bread, index}) {
           <input type='submit' value="DELETE"/>
         </form>
 
+        <a href={`/breads/${index}/edit`}>
+          <button>Edit</button>
+          </a>
+
+
 
         <li><a href="/breads">Go Home</a></li>
       </Default>
